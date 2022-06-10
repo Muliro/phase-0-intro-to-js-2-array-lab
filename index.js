ff2e2334f4864f33ec45dffc0edc6ce9b0d4d4cb
michael muliro <michael.muliro@student.moringaschool.com>
2022-06-10 17:59:05 +0300

    Done.

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 // Write your solution here!
 const cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat(name){
+function destructivelyAppendCat(name){      //destructively appends a cat
     cats.push(name);
 }
 
-function destructivelyPrependCat(name){
+function destructivelyPrependCat(name){ //destructively prepends a cat
     cats.unshift(name);
 }
 
-function destructivelyRemoveLastCat(){
+function destructivelyRemoveLastCat(){ //destructively removes the last cat
 cats.pop();
 }
 
-function destructivelyRemoveFirstCat(){
+function destructivelyRemoveFirstCat(){ //destructively removes the first cat
     cats.shift();
 }
 
-function appendCat(name){
+function appendCat(name){    //uses the spread operator to append a cat
     return [...cats, name];
 }
 
-function prependCat(name){
+function prependCat(name){ //uses the spread operator to prepend a cat
     return [name, ...cats];
 }
 
-function removeLastCat(){
+function removeLastCat(){   // uses slice to non-destructively remove last cat
   return cats.slice(0, -1);
 }
 
-function removeFirstCat(){
+function removeFirstCat(){ //uses slice to non-destructively remove first cat
     return cats.slice(1);
 }
 
